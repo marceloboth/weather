1280d31e15342542cdfa3996cee5d7036bb20655
Marcelo J. Both <marcelo.both@gmail.com>
2016-11-03 21:08:56 -0200

    Add temperature

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = "6d9af618e85f54b450dd160b858ca58c";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},ca`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
